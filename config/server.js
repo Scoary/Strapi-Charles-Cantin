18de730818a600934fa9b74c99f8b404d68befb6
Flavien Groslevin <fgroslevin@gmail.com>
2021-12-20 13:30:21 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bdf48a7b834487a541c763129dc7f8fe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'be24b928-908d-47cd-b517-fbbf1a6ae696'
+};
